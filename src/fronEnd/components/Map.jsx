ec018088879363e84cc9bb0db08ba72b9d93a355
mhmd.shorafa <mhmd_shorafa@hotmail.com>
2017-06-03 20:52:47 +0300

    final edits

--- src/fronEnd/components/Map.jsx
+++ src/fronEnd/components/Map.jsx
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
 import drop_ins from '../../../public/drop_ins.js';
 import shelters from '../../../public/shelters.js';
-import water from '../../../public/water.js';
-import youth from '../../../public/youth.js';
 import List from './list.jsx';
 
 class Map extends Component {
@@ -14,135 +12,94 @@ class Map extends Component {
       map: null,
       maps: null,
       markers: [],
-      centerLng:-79.37840775,
-      centerLat:43.66014526
+      content: '',
+      prevMarker: null
     }
   }
 
-  matchMarker(lat, lng) {
-    const matchMarker = this.state.markers.filter((marker) => {
-      return marker.getPosition().lat() == lat;
-    })
-    if (matchMarker.length > 0) {
-      this.setState({
-        centerLng : matchMarker[0].getPosition().lng(),
-        centerLng : matchMarker[0].getPosition().lat()
-      });
-      this.state.maps.event.trigger(matchMarker[0], "click");
-    }
+  matchMarker(id, address, name) {
+    this.setState({
+      content: '<div>' + address + '<br />' + name + '</div>'
+    }, () => {
+      (this.state.prevMarker != null)
+        ? this.state.prevMarker.setAnimation(null)
+        : console.log('ss')
+      const matchMarker = this.state.markers.filter((marker) => {
+        return marker.get('id') == id;
+      })
+      if (matchMarker.length > 0) {
+        this.state.maps.event.trigger(matchMarker[0], "click");
+      }
+    });
   }
 
   render() {
-    var drops = [];
+
+    var allMarkers = [];
+    var infowindow;
+
     return (
       <div>
         <div className="map-wrp">
-          <GoogleMapReact bootstrapURLKeys={{
-            key: 'AIzaSyDFeKjIbY0DFXhuE_iRx0rAGTOTUPjcXFs'
-          }} onGoogleApiLoaded={({map, maps}) => {
-            drop_ins.features.map((data) => {
-              const infowindow = new maps.InfoWindow({
-                content: '<div>' + data.properties.Address + '<br />' + data.properties.NAME + '</div>'
-              });
-              const marker = new maps.Marker({
-                map: map,
-                draggable: false,
-                animation: maps.Animation.DROP,
-                position: {
-                  lat: data.geometry.coordinates[1],
-                  lng: data.geometry.coordinates[0]
-                }
-              });
-              marker.addListener('click', function() {
+          <GoogleMapReact bootstrapURLKeys={{key: 'AIzaSyDFeKjIbY0DFXhuE_iRx0rAGTOTUPjcXFs'}}
+            onGoogleApiLoaded={({map, maps}) => {
+
+              drop_ins.features.map((data) => {
+                const marker = new maps.Marker({
+                  map: map,
+                  draggable: true,
+                  animation: maps.Animation.DROP,
+                  position: {
+                    lat: data.geometry.coordinates[1],
+                    lng: data.geometry.coordinates[0]
+                  }
+                });
+
+              marker.addListener('click', () => {
+                this.setState({prevMarker: marker});
+                infowindow.close();
+                infowindow = new maps.InfoWindow({content: this.state.content});
                 infowindow.open(map, marker);
-                if (marker.getAnimation() !== null) {
-                  marker.setAnimation(null);
-                } else {
-                  marker.setAnimation(maps.Animation.BOUNCE);
-                }
+                marker.setAnimation(maps.Animation.BOUNCE);
               });
+
               marker.set("id", data.properties.OBJECTID);
-              drops.push(marker);
+              allMarkers.push(marker);
             });
-            water.features.map((data) => {
-              const infowindow = new maps.InfoWindow({
-                content: '<div>' + data.properties.ASSET_TYPE + '<br />' + data.properties.ASSET_NAME + '</div>'
-              });
-              const marker = new maps.Marker({
-                map: map,
-                draggable: false,
-                animation: maps.Animation.DROP,
-                position: {
-                  lat: data.geometry.coordinates[1],
-                  lng: data.geometry.coordinates[0]
-                }
-              });
-              marker.addListener('click', function() {
-                if (marker.getAnimation() !== null) {
-                  marker.setAnimation(null);
-                } else {
-                  marker.setAnimation(maps.Animation.BOUNCE);
-                }
-                infowindow.open(map, marker);
-              });
-              marker.set("id", data.properties.OBJECTID);
-              drops.push(marker);
-            });
+
+
             shelters.features.map((data) => {
-              const infowindow = new maps.InfoWindow({
-                content: '<div>' + data.properties.ADDRESS_FU + '<br />' + data.properties.TYPE2 + '</div>'
-              });
+              infowindow = new maps.InfoWindow({content: this.state.content});
               const marker = new maps.Marker({
                 map: map,
-                draggable: false,
+                draggable: true,
                 animation: maps.Animation.DROP,
                 position: {
                   lat: data.geometry.coordinates[1],
                   lng: data.geometry.coordinates[0]
                 }
               });
-              marker.addListener('click', function() {
-                if (marker.getAnimation() !== null) {
-                  marker.setAnimation(null);
-                } else {
-                  marker.setAnimation(maps.Animation.BOUNCE);
-                }
+
+              marker.addListener('click', () => {
+                this.setState({prevMarker: marker});
+                infowindow.close();
+                marker.setAnimation(maps.Animation.BOUNCE);
                 infowindow.open(map, marker);
               });
+
               marker.set("id", data.properties.OBJECTID);
-              drops.push(marker);
+              allMarkers.push(marker);
             });
-            youth.features.map((data) => {
-              const infowindow = new maps.InfoWindow({
-                content: '<div>' + data.properties.AGENCY_NAM + '<br />' + data.properties.HOURS + '<br />' + data.properties.ACCESSIBIL + '</div>'
-              });
-              const marker = new maps.Marker({
-                map: map,
-                draggable: false,
-                animation: maps.Animation.DROP,
-                position: {
-                  lat: data.geometry.coordinates[1],
-                  lng: data.geometry.coordinates[0]
-                }
-              });
-              marker.addListener('click', function() {
-                if (marker.getAnimation() !== null) {
-                  marker.setAnimation(null);
-                } else {
-                  marker.setAnimation(maps.Animation.BOUNCE);
-                }
-                infowindow.open(map, marker);
-              });
-              drops.push(marker);
-            });
-            this.setState({map: map, maps: maps, markers: drops});
-          }} defaultCenter={{
-            lat: this.state.centerLat,
-            lng: this.state.centerLng
-          }} defaultZoom={16}></GoogleMapReact>
+            this.setState({map: map, maps: maps, markers: allMarkers});
+          }}
+
+          defaultCenter={{
+            lat: 43.66014526,
+            lng: -79.37840775
+          }} defaultZoom={15}></GoogleMapReact>
         </div>
 
-        <List drop={drop_ins.features} water={water.features} shelters={shelters.features} youth={youth.features} matchMarker={this.matchMarker}/>
+        <List drop={drop_ins.features} shelters={shelters.features} matchMarker={this.matchMarker}/>
 
       </div>
     );
--- src/fronEnd/components/list.jsx
+++ src/fronEnd/components/list.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const List = ({drop,water,shelters,youth,matchMarker}) => {
+const List = ({drop,water,shelters,matchMarker}) => {
   const dropItems = drop.map((item) => {
     return (
       <li key = {item.properties.OBJECTID} className="item" onMouseEnter = {() =>
           {
-            matchMarker(item.geometry.coordinates[1],item.geometry.coordinates[0]);
+            matchMarker(item.properties.OBJECTID,item.properties.Address,item.properties.NAME);
           }
         }>
         <h3>{item.properties.Address}</h3>
@@ -16,7 +16,7 @@ const List = ({drop,water,shelters,youth,matchMarker}) => {
   const sheltersItem = shelters.map((item) => {
     return (
       <li key = {item.properties.OBJECTID}  className="item" onMouseEnter = {() => {
-        matchMarker(item.geometry.coordinates[1],item.geometry.coordinates[0]);
+        matchMarker(item.properties.OBJECTID,item.properties.ADDRESS_FU,item.properties.TYPE2);
       }
     }>
         <h3>{item.properties.ADDRESS_FU}</h3>
@@ -24,35 +24,11 @@ const List = ({drop,water,shelters,youth,matchMarker}) => {
       </li>
     );
   });
-  const youthItem = youth.map((item) => {
-    return (
-      <li key = {item.properties.OBJECTID}  className="item" onMouseEnter = {() => {
-        matchMarker(item.geometry.coordinates[1],item.geometry.coordinates[0]);
-      }
-    }>
-        <h3>{item.properties.AGENCY_NAM}</h3>
-        <h4>{item.properties.HOURS}</h4>
-        <h4>{item.properties.ACCESSIBIL}</h4>
-      </li>
-    );
-  });
-// const waterItem = water.map((item) => {
-//   return (
-//     <li key = {item.properties.OBJECTID} className="item" onMouseEnter = {() => {
-//       matchMarker(item.geometry.coordinates[1],item.geometry.coordinates[0]);
-//     }
-//   }>
-//       <h3>{item.properties.ASSET_TYPE}</h3>
-//       <h4>{item.properties.ASSET_NAME}</h4>
-//     </li>
-//   );
-// });
+
   return (
     <ul className="item-wrp">
       {dropItems}
       {sheltersItem}
-      {youthItem}
-      
     </ul>
   );
 }
